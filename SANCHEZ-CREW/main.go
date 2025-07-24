83a0cee185c7f2b59da90cc5fae9f25f05300d38
Aaron Sanchez <saarong12@gmail.com>
2025-07-24 01:52:16 -0500

    Added initial starting point for Sanchez-Crew landing page demo.

---
+++ SANCHEZ-CREW/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"SANCHEZ-CREW/internal/templates"
+
+	"github.com/a-h/templ"
+)
+
+func main() {
+	landing := templates.LandingPage()
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.Handle("/", templ.Handler(landing))
+
+	fmt.Println("Listening on :3000")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		log.Printf("Error listening: %v", err)
+	}
+}
