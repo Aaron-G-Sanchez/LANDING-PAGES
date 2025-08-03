c3a281f6dd4af71bcfeadf2544bb1941891910df
Aaron Sanchez <saarong12@gmail.com>
2025-08-02 22:01:23 -0500

    Added stylings to header template.

--- SANCHEZ-CREW/internal/components/header_templ.go
+++ SANCHEZ-CREW/internal/components/header_templ.go
@@ -29,7 +29,7 @@ func HeaderTemplate() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<header class=\"heading\"><h1>Sanchez Crew</h1></header>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<header class=\"heading\"><div class=\"title\"><h2>The Sanchez Crew</h2></div><div class=\"hero\"><h1>Lawncare <br>&amp Landscape Management</h1></div></header>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- SANCHEZ-CREW/internal/templates/landing-page_templ.go
+++ SANCHEZ-CREW/internal/templates/landing-page_templ.go
@@ -31,7 +31,7 @@ func LandingPage() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link rel=\"stylesheet\" href=\"/static/css/main.css\"><title>The Sanchez Crew</title></head><body>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link rel=\"stylesheet\" href=\"/static/css/main.css\"><link rel=\"preconnect\" href=\"https://fonts.googleapis.com\"><link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin><link href=\"https://fonts.googleapis.com/css2?family=Open+Sans&display=swap\" rel=\"stylesheet\"><title>The Sanchez Crew</title></head><body>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
